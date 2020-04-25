fe9a0046b1f4ee32f43afa7af417b4837ece0db6
Maicon tadeu <maicon.silva@lal.infnet.edu.br>
2020-04-25 18:48:42 -0300

    Correções no portugues

--- Exercício 13.py
+++ Exercício 13.py
@@ -2,7 +2,7 @@
 # sendo que os comprimentos do raio e a altura são informados pelo usuário. 
 # O volume é calculado multiplicando-se pi, o raio ao quadrado e a altura.
 # Mostra ao usuário os valores necessários.
-print("Para fazer-mos o cálculo da área da caixa d'água cilindrica são necessários os valores do raio e a altura.")
+print("Para fazer-mos o cálculo do volume da caixa d'água cilindrica são necessários os valores do raio e a altura.")
 # Importar o módulo de funções matemáticas.
 import math
 # Variáveis de raio e altura da caixa.
@@ -12,4 +12,4 @@ altura_caixa = float(input("Insira o valor da altura da caixa: "))
 volume_caixa = math.pi * (raio_caixa**2) * altura_caixa
 volume_caixa = round(volume_caixa, 4)
 # Mostra o resultado.
-print(f"O valor da área da caixa d'água é {volume_caixa}.")
+print(f"O valor do volume da caixa d'água é {volume_caixa}.")
